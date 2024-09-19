3c6ba1a3378f8d7353b0ec9414f09fe10e6090d9
Illya Vakulenko <vacodenko@gmail.com>
2024-09-20 00:09:06 +0300

    Task 02

---
+++ script.js
@@ -0,0 +1,9 @@
+const NOW_YEAR = new Date().getFullYear();
+console.log(NOW_YEAR);
+
+const whoami = (name, year_of_birth = 1991) => {
+  return `Я ${name}, мені ${NOW_YEAR - year_of_birth} років`;
+}
+
+console.log(whoami("Ukraine"))
+console.log(whoami("Illya", 2005))
